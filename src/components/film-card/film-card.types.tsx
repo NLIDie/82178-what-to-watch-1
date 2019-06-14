893574800bf517a5ee070ae90ec2d13f01f3fb71
kkostikov <kkostikov@pushwoosh.com>
2019-06-14 21:41:37 +0300

    Added VideoPlayer in FilmCard

--- src/components/film-card/film-card.types.tsx
+++ src/components/film-card/film-card.types.tsx
@@ -3,7 +3,8 @@ import * as React from "react";
 export type TProps = {
   readonly id: number;
   readonly title: string;
-  readonly preview: string;
+  readonly poster: string;
+  readonly previewVideo: string;
   readonly onPlay: (
     event: React.SyntheticEvent<HTMLElement>,
     filmId: number
